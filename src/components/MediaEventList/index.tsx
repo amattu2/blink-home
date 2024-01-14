0aeb622c54f2379e65a593ec033a55783da31859
Alec M. <alecmattu1234@gmail.com>
2024-01-14 10:57:39 -0500

    feat: Add thumbnail to events and add pagination

--- src/components/MediaEventList/index.tsx
+++ src/components/MediaEventList/index.tsx
@@ -1,46 +1,80 @@
 import React, { FC, useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { Divider, List, Skeleton, Tag } from "antd";
+import { Divider, List, Tag } from "antd";
 import { getChangedMedia } from "@/api/actions";
 import {
   formatYYYYMMDD,
   getDateNDaysAgo,
   getEventSource,
 } from "@/utils/dashboard";
+import { useLocalStorage } from "usehooks-ts";
+import { STORAGE_KEYS } from "@/config/STORAGE_KEYS";
+import Thumbnail from "../Thumbnail";
 
 type Props = {
   since?: Date;
 };
 
+const PAGINATION_SIZE = 5;
+
+const ListItem: FC<{ event: MediaEvent }> = ({ event }) => (
+  <List.Item key={event.id}>
+    <List.Item.Meta
+      title={event.device_name}
+      description={
+        <>
+          <p>{`Recorded at ${event.created_at}`}</p>
+          <Tag>{event.network_name}</Tag>
+          <Tag>{getEventSource(event.source)}</Tag>
+        </>
+      }
+    />
+    <Thumbnail src={event.thumbnail} width={200} height={112} alt="Thumbnail" />
+  </List.Item>
+);
+
 /**
  * Basic list of media events (i.e. Clips)
  *
- * @param {Props} { limit }
  * @returns {React.FC<Props>}
  */
 const MediaEventList: FC<Props> = ({ since = getDateNDaysAgo(7) }) => {
   const [loading, setLoading] = useState<boolean>(false);
-  const [data, setData] = useState<MediaEvent[]>([]);
+  const [data, setData] = useLocalStorage<MediaEvent[]>(
+    STORAGE_KEYS.media_events,
+    [],
+  );
+  const [paginatedData, setPaginatedData] = useState<MediaEvent[]>(
+    data.length > 0 ? data.slice(0, PAGINATION_SIZE) : [],
+  );
+  const [hasMore, setHasMore] = useState<boolean>(true);
 
   const sinceDate = formatYYYYMMDD(since);
 
   const loadMore = async () => {
-    if (loading) {
+    if (loading || data.length === 0) {
+      return;
+    }
+    if (paginatedData.length >= data.length) {
       return;
     }
 
-    setLoading(true);
-
-    const d = await getChangedMedia(sinceDate, 1);
-    if (d.status === "ok") {
-      setData([...data, ...d.media]);
-    }
-
-    setLoading(false);
+    setPaginatedData(data.slice(0, paginatedData.length + PAGINATION_SIZE));
+    setHasMore(paginatedData.length < data.length);
   };
 
   useEffect(() => {
-    loadMore();
+    setLoading(true);
+
+    (async () => {
+      const d = await getChangedMedia(sinceDate, 1);
+      if (d.status === "ok") {
+        setData(d.media);
+        setPaginatedData(d.media.slice(0, PAGINATION_SIZE));
+        setHasMore(d.media.length > PAGINATION_SIZE);
+      }
+      setLoading(false);
+    })();
   }, []);
 
   return (
@@ -53,29 +87,17 @@ const MediaEventList: FC<Props> = ({ since = getDateNDaysAgo(7) }) => {
       }}
     >
       <InfiniteScroll
-        dataLength={data.length}
+        dataLength={paginatedData.length}
+        hasMore={hasMore}
         next={loadMore}
-        hasMore={false}
-        loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
-        endMessage={<Divider plain>No more events</Divider>}
+        loader={null}
+        endMessage={<Divider plain>Nothing else to see here...</Divider>}
         scrollableTarget="mediaEventList"
       >
         <List
-          dataSource={data}
-          renderItem={(event) => (
-            <List.Item key={event.id}>
-              <List.Item.Meta
-                // TODO: include the thumbnail and live player
-                title={event.device_name}
-                description={
-                  <>
-                    <p>{`Recorded at ${event.created_at}`}</p>
-                    <Tag>{event.network_name}</Tag>
-                    <Tag>{getEventSource(event.source)}</Tag>
-                  </>
-                }
-              />
-            </List.Item>
+          dataSource={paginatedData}
+          renderItem={(event: MediaEvent) => (
+            <ListItem key={event.id} event={event} />
           )}
         />
       </InfiniteScroll>
--- src/config/STORAGE_KEYS.ts
+++ src/config/STORAGE_KEYS.ts
@@ -1,3 +1,4 @@
 export const STORAGE_KEYS: { [key: string]: string } = {
   homescreen: "homescreen",
+  media_events: "media_events",
 };
